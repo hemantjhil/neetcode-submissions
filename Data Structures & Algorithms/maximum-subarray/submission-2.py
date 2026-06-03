53ae2484109d2dd3213740ff9b73594f657d836c
hemantjhil <32435133+hemantjhil@users.noreply.github.com>
2026-06-03 21:56:48 +0530

    Add: maximum-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-subarray/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadane's Algo
+        # Track running sum and reset to 0 when it goes negative
+        # Keep track of the maximum sum seen so far
+        i=0
+        maxSum=nums[0]
+        sum=0
+        while i<len(nums):
+            sum+=nums[i]
+            maxSum=max(sum,maxSum)
+            if sum<0:
+                sum=0
+            i+=1
+        return maxSum
+
+
+
+
+        # i=0
+        # maxSum=nums[0]
+        # sum=0
+        # while(i<len(nums)):
+        #     sum+=nums[i]
+        #     maxSum=max(sum,maxSum)
+        #     if(sum<0):
+        #         sum=0
+        #     i+=1
+        # return maxSum
+        
+        
